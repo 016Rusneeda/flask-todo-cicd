efbcf81b8fad18236ff9be895ebd12167bafdfb1
RusneedaKuwing <rusneedakuwi@gmail.com>
2025-10-19 04:03:05 +0700

    add practices

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ import os
 
 
 from flask import Flask, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from app.models import db
 from app.routes import api
 from app.config import config
+
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 
 def create_app(config_name=None):
