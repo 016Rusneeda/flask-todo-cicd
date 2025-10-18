80405ed331ce013907db1677dc5d8161182af59e
RusneedaKuwing <rusneedakuwi@gmail.com>
2025-10-19 04:17:26 +0700

    fix delete line

--- app/logging_config.py
+++ app/logging_config.py
@@ -6,13 +6,13 @@ def setup_logging(app):
     """Configure application logging"""
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.INFO)
-    
+
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
     handler.setFormatter(formatter)
-    
+
     app.logger.addHandler(handler)
     app.logger.setLevel(logging.INFO)
-    
+
     return app.logger
